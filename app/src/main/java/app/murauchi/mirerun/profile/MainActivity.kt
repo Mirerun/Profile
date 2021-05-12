ae4c378e75d106463456013750a88e5c2e172dc1
mirei <mirei.saxophone1010@gmail.com>
2021-05-12 23:06:07 +0900

    プロフィールアプリ4

--- app/src/main/java/app/murauchi/mirerun/profile/MainActivity.kt
+++ app/src/main/java/app/murauchi/mirerun/profile/MainActivity.kt
@@ -2,10 +2,25 @@ package app.murauchi.mirerun.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //画像を見えないようにする
+        profileImage.isVisible = false
+
+        //テキストを見えないようにする
+        profileCommentText.isVisible = false
+
+        //ボタンがタップされた時に
+        button1.setOnClickListener {
+            //見えるようにする
+            profileImage.isVisible = true
+            profileCommentText.isVisible = true
+        }
     }
 }
