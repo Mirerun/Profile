5c4d6717ed4fd9e35ef254129674376c3bf4290d
mirei <mirei.saxophone1010@gmail.com>
2021-05-13 00:39:29 +0900

    プロフィールアプリ5

--- app/src/main/java/app/murauchi/mirerun/profile/MainActivity.kt
+++ app/src/main/java/app/murauchi/mirerun/profile/MainActivity.kt
@@ -10,18 +10,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //画像を見えないようにする
-        profileImage.isVisible = false
-
-        //テキストを見えないようにする
-        profileCommentText.isVisible = false
-
-        //ボタンがタップされた時に
+        //ボタン１(名前)がタップされた時に
         button1.setOnClickListener {
-            //見えるようにする
-            profileImage.isVisible = true
-            profileCommentText.isVisible = true
-            //aio
+            //画像をImageViewに反映させる
+            profileImage.setImageResource(R.drawable.randy_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "名前"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "Androidメンターのランディだよ"
         }
+
+        //スポーツボタンが押された時に
+        button2.setOnClickListener {
+            //画像をImageViewに反映させる
+            profileImage.setImageResource(R.drawable.baseball_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "スポーツ"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+        }
+
+        //食べ物ボタンが押された時に
+        button3.setOnClickListener {
+            //画像をImageViewに反映させる
+            profileImage.setImageResource(R.drawable.donut_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "好きな食べ物"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "キャンディーやドーナツが大好き"
+        }
+
+        //趣味ボタンが押された時に
+        button4.setOnClickListener {
+            //画像をImageViewに反映させる
+            profileImage.setImageResource(R.drawable.gadget_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "趣味"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "ガジェットを集めて動かすこと"
+        }
+
     }
 }
